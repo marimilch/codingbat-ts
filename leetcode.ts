5ea9a49e936553b0d7c087cb74a608855b853285
Marian Amiragov <marian@eduroam-103-158.zdv.Uni-Mainz.DE>
2022-05-06 18:21:49 +0200

    Two leetcode challenges

---
+++ leetcode.ts
@@ -0,0 +1,138 @@
+import { assertEquals } from "https://deno.land/std@0.138.0/testing/asserts.ts";
+
+type romanLetters = Record<string, number>
+
+console.log("\n romanToInt")
+function romanToInt(s: string): number 
+{
+  const singleLetters: romanLetters = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  }
+
+  const doubleLetters: romanLetters = {
+    IV: 4,
+    IX: 9,
+    XL: 40,
+    XC: 90,
+    CD: 400,
+    CM: 900,
+  }
+
+  let result = 0
+
+  let lastC = ''
+  for (const c of s.split('')) {
+    if (doubleLetters[lastC + c]) {
+      result = result - singleLetters[lastC] + doubleLetters[lastC + c]
+    } else {
+      result += singleLetters[c]
+    }
+    lastC = c
+  }
+
+  return result
+}
+
+Deno.test('romanToInt', () => {
+  assertEquals(3, romanToInt('III'))
+  assertEquals(58, romanToInt('LVIII'))
+  assertEquals(1994, romanToInt('MCMXCIV'))
+})
+
+function removeDuplicates(s: string, k: number): string {
+  let touched = false
+  // const sSplit = s.split('')
+
+  do {
+    if (s.length < k) return s 
+
+    touched = false
+    let lastC = ''
+    let currentLen = 0 
+    let startIndex = 0
+
+    for (let ci = 0; ci <= s.length; ++ci) {
+      const c = s.charAt(ci) // ok if undefined
+
+      // Remove letter chain if equal to k and restart counting
+      if (currentLen === k) {
+        touched = true
+        s = s.substring(0, startIndex) + s.substring(startIndex + currentLen)
+        ci -= k
+        startIndex = ci
+        currentLen = 0
+      }
+
+      // New letter chain
+      if (lastC !== c) {
+        startIndex = ci
+        currentLen = 1
+        lastC = c
+      } else {
+        currentLen++
+      }
+    }
+  } while (touched)
+    
+
+  return s
+}
+
+Deno.test('removeDuplicates', () => {
+  assertEquals(removeDuplicates('abcd', 2), 'abcd')
+  assertEquals(removeDuplicates('deeedbbcccbdaa', 3), 'aa')
+  assertEquals(removeDuplicates('pbbcggttciiippooaais', 2), 'ps')
+  assertEquals(removeDuplicates('yfttttfbbbbnnnnffbgffffgbbbbgssssgthyyyy', 4), 'ybth')
+})
+
+type charCounts = Record<string, number>
+
+function removeDuplicates2(s: string, k: number){
+  // Phase 1: Just count
+  const csRaw: charCounts = {}
+  for (let i = 0; i < s.length; ++i){
+    const c = s.charAt(i)
+    if (!csRaw[c]) csRaw[c] = 0
+    ++csRaw[c]
+  }
+
+  // Phase 2: Only keep counts > k
+  const cs: charCounts = {}
+  for (const c in csRaw) {
+    if (csRaw[c] < k) continue
+
+    cs[c] = csRaw[c]
+  }
+
+  // Phase 3: Replace potential duplicates
+  let touched = false
+  do {
+    touched = false
+    for (const c in cs) {
+      if (cs[c] < k) continue
+
+      let toFind = ''
+      for (let i = 0; i < k; ++i) {
+        toFind += c
+      }
+      const oldS = s
+      s = s.replaceAll(toFind, '')
+      touched = oldS.length !== s.length || touched
+    }
+  } while (touched)
+
+  return s
+}
+
+Deno.test('removeDuplicates2', () => {
+  assertEquals(removeDuplicates2('abcd', 2), 'abcd')
+  assertEquals(removeDuplicates2('deeedbbcccbdaa', 3), 'aa')
+  assertEquals(removeDuplicates2('pbbcggttciiippooaais', 2), 'ps')
+  assertEquals(removeDuplicates2('yfttttfbbbbnnnnffbgffffgbbbbgssssgthyyyy', 4), 'ybth')
+})
--- warmup-1.ts
+++ warmup-1.ts
@@ -43,3 +43,54 @@ function diff21(n: number): number {
 console.log(diff21(19), 2)
 console.log(diff21(10), 11)
 console.log(diff21(21), 0)
+
+console.log("\n icyHot")
+
+function icyHot(temp1: number, temp2: number): boolean {
+  if (temp1 > 100 && temp2 < 0) return true
+  if (temp2 > 100 && temp1 < 0) return true
+
+  return false
+}
+
+console.log(icyHot(120, -1), true)
+console.log(icyHot(-1, 120), true)
+console.log(icyHot(2, 120), false)
+
+console.log("\n lastDigit")
+
+function lastDigit(a: number, b: number): boolean {
+  return a % 10 === b % 10
+}
+
+console.log(lastDigit(7, 17), true)
+console.log(lastDigit(6, 17), false)
+console.log(lastDigit(3, 113), true)
+
+console.log("\n endUp")
+
+// Note: end index of substring excludes itself 
+function endUp(str: string): string {
+  return (
+    str.substring(0, str.length - 3) 
+    + str.substring(str.length - 3, str.length).toLocaleUpperCase()
+  )
+}
+
+console.log(endUp("Hello"), "HeLLO")
+console.log(endUp("hi there"), "hi thERE")
+console.log(endUp("hi"), "HI")
+
+console.log("\n everyNth")
+
+function everyNth(str: string, n: number): string {
+  return str.split('').reduce( (accStr, c, i) => {
+    if (i % n === 0) return accStr + c
+    return accStr
+  })
+}
+
+console.log(everyNth("Miracle", 2), "Mrce")
+console.log(everyNth("abcdefg", 2), "aceg")
+console.log(everyNth("abcdefg", 3), "adg")
+
