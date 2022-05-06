910dc1af0aa4257db22befefba2812c48d1653a6
Marian Amiragov <marian@eduroam-103-158.zdv.Uni-Mainz.DE>
2022-05-06 20:21:36 +0200

    leetcode solution

--- leetcode-interview.ts
+++ leetcode-interview.ts
@@ -1,56 +1,30 @@
 import { assertEquals } from "https://deno.land/std@0.138.0/testing/asserts.ts"
 
-function arrayShiftFromRight(arr: Array<number>, index: number, shift: number): void
-{
-  for (let i = index; i < arr.length - shift; i++) {
-    arr[i] = arr[i + shift]
-  }
-
-  for (let i = arr.length - shift; i < arr.length; i++) {
-    arr[i] = -1
-  }
-}
-
 // Should neither return new array nor change the arrays length
-function removeDuplicatesFromSortedArray(arr: Array<number>): void
+function removeDuplicatesFromSortedArray(arr: Array<number>): Array<number>
 {
-  if (arr.length < 2) return
+  if ( arr.length < 2 ) return arr
 
-  let lastN = arr[0]
-  let currentLength = 1
-  let startIndex = 0
-
-  for (let i = 1; i < arr.length; i++) {
-    const n = arr[i]
-    
-    // new row
-    if (lastN !== n)
-    {
-      arrayShiftFromRight(
-        arr,
-        startIndex,
-        currentLength - 1
-      )
-
-      i -= currentLength - 1
-      currentLength = 0
-      startIndex = i
+  let j = 0 // what to write at this index (slow runner)
+  for (let i = 1; i < arr.length; ++i) 
+  {
+    if (arr[i] !== arr[j]) {
+      ++j
+      arr[j] = arr[i]
     }
-
-    lastN = n
-    ++currentLength
   }
+
+  arr.length = j + 1
+
+  return arr
 }
 
 Deno.test('removeDuplicatesFromSortedArray', () => {
+  const arr0 = [1, 1]
   const arr1 = [1, 1, 2]
   const arr2 = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
-  const arr1Expected = [1, 2, -1]
-  const arr2Expected = [0, 1, 2, 3, 4, -1, -1, -1, -1, -1]
 
-  removeDuplicatesFromSortedArray(arr1)
-  removeDuplicatesFromSortedArray(arr2)
-  
-  assertEquals(arr1, arr1Expected)
-  assertEquals(arr2, arr2Expected)
+  assertEquals(removeDuplicatesFromSortedArray(arr0), [1])
+  assertEquals(removeDuplicatesFromSortedArray(arr1), [1, 2])
+  assertEquals(removeDuplicatesFromSortedArray(arr2), [0, 1, 2, 3, 4])
 })
